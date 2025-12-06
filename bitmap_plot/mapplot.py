8c80d59ffba5471d41717941012d6dadc5b7d1c8
schnederIT <crivcianschialexei@gmail.com>
2025-12-06 20:45:24 +0200

    [dev] changed from single window, to multiple plot windows

--- bitmap_plot/mapplot.py
+++ bitmap_plot/mapplot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-import numpy as np # Already imported, but good practice
+import numpy as np
 
-def plot_matrix_color_scale(matrix: np.ndarray, title: str, cmap_name: str = 'viridis'):
+def plot_matrix_color_scale(matrix: np.ndarray, title: str, cmap_name: str = 'viridis'): # plot style can be "inferno", "gray", ...
 
     plt.figure(figsize=(8, 6)) 
     plt.imshow(matrix, cmap=cmap_name, interpolation='none')
@@ -12,23 +12,4 @@ def plot_matrix_color_scale(matrix: np.ndarray, title: str, cmap_name: str = 'vi
     plt.xlabel("X-Coordinate (Column)")
     plt.ylabel("Y-Coordinate (Row)")
     
-    plt.show()
-
-# if __name__ == '__main__':
-#     x, y = np.meshgrid(np.linspace(0, 10, 8), np.linspace(0, 10, 8))
-#     dummy_data = np.sin(x) * np.cos(y) * 127 + 127
-#     dummy_data = dummy_data.astype(np.uint8) # Convert to 0-255 scale
-    
-#     result_diff_image_array = dummy_data 
-
-#     plot_matrix_color_scale(
-#         result_diff_image_array, 
-#         "Differential Dynamics Map (I_DIFF)", 
-#         cmap_name='inferno'
-#     )
-
-#     plot_matrix_color_scale(
-#         dummy_data, # Replace with your original IR map
-#         "Original IR Map (Simulated)", 
-#         cmap_name='gray'
-#     )
+    # plt.show() # must be added after all windows definitions for multiple window show
--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 from stat_analysis.dummy_bitmaps import X, Y
 from stat_analysis.grad_analysis import gradient_difference
 from bitmap_plot.mapplot import plot_matrix_color_scale
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     Result = gradient_difference(X, Y)
     plot_matrix_color_scale(X, "X")
     plot_matrix_color_scale(Y, "Y")
-    plot_matrix_color_scale(Result, "Gradient difference")
+
+    plot_matrix_color_scale(Result, "Gradient difference X-Y")
+    plot_matrix_color_scale(gradient_difference(X, X), "Gradient diff X-X")
+
+    plt.show()
